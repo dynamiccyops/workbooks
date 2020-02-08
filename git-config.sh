db55fa2744abdc46c38d4dc91b6842456359f9e8
Eddie Rangel <eddie@dynamiccyops.com>
2020-02-08 00:55:24 -0800

    Add scripts

---
+++ git-config.sh
@@ -0,0 +1,13 @@
+# Install open-vm-tools-desktop
+# Eddie Rangel
+# Dynamic CyOps LLC
+# Copyright 2020
+# MIT License
+
+
+#!/bash/bin
+
+echo "config the git"
+git config --global user.email "joe@theclash.com"
+git config --global user.name "Joe Strummer"
+git config --global credential.helper cache
---
+++ install-open-vm-tools-desktop.sh
@@ -0,0 +1,11 @@
+# Install open-vm-tools-desktop
+# Eddie Rangel
+# Dynamic CyOps LLC
+# Copyright 2020
+# MIT License
+
+
+#!/bash/bin
+
+echo "Installing open-vm-tools-desktop"
+sudo apt-get install open-vm-tools-desktop
